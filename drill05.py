b4975525bba76bcf8e0d19a5b29231f93d61c767
hndd0725 <hndd0725@naver.com>
2023-10-04 16:35:53 +0900

    캐릭터 마우스 이동 기능 삭제

--- drill05.py
+++ drill05.py
@@ -16,8 +16,7 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
