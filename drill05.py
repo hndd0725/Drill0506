4d17f46e1afdaded9ec3fef57d4b0ebc20b11f50
hndd0725 <hndd0725@naver.com>
2023-10-04 17:22:03 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t값이 1를 넘으면 다시, 새로운 손화살표가 랜덤 위치에 만들어짐

--- drill05.py
+++ drill05.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import random
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 
@@ -7,7 +8,7 @@ def load_resources():
     global TUK_ground, character
     global arrow
 
-    arrow=load_image('hand_arrow.png')
+    arrow = load_image('hand_arrow.png')
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
 
@@ -27,42 +28,50 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx,hy
+    global hx, hy
     global sx, sy
     global t
     global action
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-    action=3
-    sx,sy=cx,cy#p1:시작점
+    action = 3
+    set_new_target_arrow()
 
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy  # p1:시작점
     # hx,hy=50,50
-    hx,hy=random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)#p2:끝점
-    t=0.0
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2:끝점
+    t = 0.0
 
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx,hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
 
 def update_world():
     global frame
-    global cx,cy
+    global cx, cy
     global t
     global action
 
     frame = (frame + 1) % 8
-    action=1 if cx<hx else 0
+    action = 1 if cx < hx else 0
 
-    if t<=1.0:
-        cx=(1-t)*sx+t*hx#cx는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
-        cy=(1-t)*sy+t*hy
-        t+=0.001
+    if t <= 1.0:
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx,cy=hx,hy#캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+        set_new_target_arrow()
+
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
